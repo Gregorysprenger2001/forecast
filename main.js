cfb984e97f73087136e96dbcaa7a6d65e4105b2f
Gregory Sprenger <gregory.sprenger@student.uibk.ac.at>
2025-05-15 17:46:21 +0200

    wettervorhersage

--- main.js
+++ main.js
@@ -41,14 +41,17 @@ async function showForecast(latlng) {
 
         //Popup erzeugen
 
+    let details= jsondata.properties.timeseries[0].data.instant.details;
+    let timestamp =  new Date(jsondata.properties.meta.updated_at)
     let markup= `
+    <h3> Wettervorhersage für ${timestamp.toLocalString()}
     <ul>
-    <li>Luftdruck (hpa): air_pressure_at_sea_level</li>
-    <li>Lufttemperatur (°C): air_temperature</li>
-    <li>Bevölkerungsgrad (%): cloud_area_fractions</li>
-    <li>Luftfeuchtigkeit (%): relative_humidity</li>
-    <li>Windrichtung (°): wind_from_direction</li>
-    <li>Windgeschwindigkeit (km/h): wind_speed</li>
+    <li>Luftdruck (hpa): ${details.air_pressure_at_sea_level}</li>
+    <li>Lufttemperatur (°C): ${details.air_temperature}</li>
+    <li>Bevölkerungsgrad (%): ${details.cloud_area_fraction}</li>
+    <li>Luftfeuchtigkeit (%): ${details.relative_humidity}</li>
+    <li>Windrichtung (°): ${details.wind_from_direction}</li>
+    <li>Windgeschwindigkeit (km/h): ${details.wind_speed}</li>
     </ul>
     `;
 
